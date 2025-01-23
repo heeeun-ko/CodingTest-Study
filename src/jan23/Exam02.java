0bd4edb6e47d2c024dceac12afc459cce6c406d3
KHE <99ant@naver.com>
2025-01-23 21:59:46 +0900

    Jan23

--- src/jan23/Exam02.java
+++ src/jan23/Exam02.java
@@ -1,31 +1,32 @@
 package jan23;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.Collections;
 
 public class Exam02 {
-//    public static int[] solution(int[] arr) {
-//        Arrays.sort(arr);
-//
-//        int[] tempArr = new int[arr.length];
-//        tempArr[0] = arr[0];
-//
-//        for (int i = 1; i < arr.length; i++) {
-//            for (int j = 0; j <= i; j++) {
-//                if (arr[i] != arr[j]){
-//                    tempArr[i] =
-//                }
-//            }
-//        }
-//        return arr;
-//    }
+    public static int[] solution(int[] arr) {
+
+        Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
+        //Integer [] result2 = Arrays.stream(arr).boxed().distinct().sorted(Collections.reverseOrder()).toArray(Integer []::new);
+
+        Arrays.sort(result, Collections.reverseOrder());
+
+//        int[] arr2 = {1, 2, 2, 3};
+//        int[] result2 = Arrays.stream(arr)
+//                .distinct() // IntStream에서 중복 제거
+//                .toArray();  // 기본형 배열(int[]) 생성
+//        //기본 자료형 : int, String, double
+//        System.out.println(Arrays.toString(result)); // [1, 2, 3]
+
+        return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+    }
 
     public static void main(String[] args) {
-//        int[] arr = {4, 2, 2, 1, 4, 3, 7, 7, 3, 58};
-//
-//        System.out.println(solution(arr));
-        String[] arr = new String[]{"a", "b", "c"};
-        Stream<String> stream = Arrays.stream(arr);
-        System.out.println(stream);
+        int[] arr = {4, 2, 2, 1, 4, 3, 7, 7, 3, 58};
+
+        int[] arrResult = solution(arr);
+        for (int r : arrResult) {
+            System.out.print(r + ", ");
+        }
     }
 }
