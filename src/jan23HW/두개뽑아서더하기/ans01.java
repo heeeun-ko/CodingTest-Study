cc2b07d4adc7cc8c79fff6df65905f3d5160fcd6
KHE <99ant@naver.com>
2025-02-01 02:23:12 +0900

    5. 두 개 뽑아서 더하기

---
+++ src/jan23HW/두개뽑아서더하기/ans01.java
@@ -0,0 +1,35 @@
+package jan23HW.두개뽑아서더하기;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ans01 {
+    static List<Integer> solution(int[] numbers) {
+        int[] numbers2 = numbers.clone();
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers2.length; j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    res.add(numbers[i] + numbers2[j]);
+                }
+            }
+        }
+
+        List<Integer> newRes = res.stream().distinct().collect(Collectors.toList());
+        Collections.sort(newRes);
+
+        return newRes;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 1, 3, 4, 1};
+
+        System.out.println(solution(numbers));
+    }
+}
