bc821882abc52a94b6c0d085f1fc3892a4221d42
KHE <99ant@naver.com>
2025-02-14 21:58:22 +0900

    주격가격 스택~!

---
+++ src/feb13/주가격/Ans03.java
@@ -0,0 +1,33 @@
+package feb13.주가격;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Ans03 {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+
+        int[] result = solution(prices);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] solution(int[] prices) {
+        int[] result = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int j = i; j < prices.length; j++) {
+                if (prices[i] <= prices[j]) {
+                    stack.push(prices[i]);
+                } else {
+                    if (i != prices.length - 1) {
+                        stack.push(prices[i]);
+                    }
+                    break;
+                }
+            }
+            result[i] = stack.size() - 1;
+        }
+        return result;
+    }
+}
