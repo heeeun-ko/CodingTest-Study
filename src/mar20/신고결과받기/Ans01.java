47ce95f8a680660f3431c9735bbe9fdaba5efe32
KHE <99ant@yonsei.ac.kr>
2025-03-20 22:20:29 +0900

    중복제거 변경 ->Set

--- src/mar20/신고결과받기/Ans01.java
+++ src/mar20/신고결과받기/Ans01.java
@@ -17,30 +17,33 @@ public class Ans01 {
 
     static int[] solution(String[] id_list, String[] report, int k) {
         HashMap<String, Set<String>> reportMap = new HashMap<>();
-        HashMap<String, Integer> reprotCnt = new HashMap<>();
+        HashMap<String, Integer> reportCnt = new HashMap<>();
         Set<String> kReportSet = new HashSet<>();
 
         //중복 제거
-        String[] newReport = Arrays.stream(report).distinct().toArray(String[]::new);
+//        String[] newReport = Arrays.stream(report).distinct().toArray(String[]::new);
+        Set<String> newReport = new HashSet<>(Arrays.asList(report));
 
         for (String r : newReport) {
-            String userId = r.split(" ")[0];
-            String reportId = r.split(" ")[1];
+            String[] parts = r.split(" ");
+            String userId = parts[0];
+            String reportId = parts[1];
 
             //reportMap
             reportMap.computeIfAbsent(userId, s -> new HashSet<>()).add(reportId);
 
             //reportCnt
-            reprotCnt.put(reportId, reprotCnt.getOrDefault(reportId, 0) + 1);
+            reportCnt.put(reportId, reportCnt.getOrDefault(reportId, 0) + 1);
 
             //k번 이상 신고 당할 경우
-            if (reprotCnt.get(reportId) >= k) {
+            if (reportCnt.get(reportId) >= k) {
                 kReportSet.add(reportId);
             }
         }
 
-        int[] mail = new int[id_list.length];
+        int[] mail = new int[id_list.length]; //{"muzi", "frodo", "apeach", "neo"}
 
+        //reportMap과 kReportSet 비교
         for (int i = 0; i < id_list.length; i++) {
             int cnt = 0;
             String userId = id_list[i];
