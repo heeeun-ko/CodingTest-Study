843f2bc33ec29793f2dc025176a0c4cacc20b2df
KHE <99ant@yonsei.ac.kr>
2025-06-13 15:19:07 +0900

    역순정렬 stream 사용

--- src/lv0/배열/진료순서정하기/Ans01.java
+++ src/lv0/배열/진료순서정하기/Ans01.java
@@ -1,30 +1,32 @@
 package lv0.배열.진료순서정하기;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Ans01 {
     public static void main(String[] args) {
         int[] emergency = {30, 10, 23, 6, 100};
-        int[] answer = Solution.solution(emergency);
+        int[] answer = solution(emergency);
         System.out.println(Arrays.toString(answer));
     }
-}
 
-class Solution {
     public static int[] solution(int[] emergency) {
         int[] emergencyCopy = emergency.clone();
         HashMap<Integer, Integer> sorting = new HashMap<>();
 
-        for (int i = 0; i < emergency.length; i++) {
-            for (int j = i + 1; j < emergency.length; j++) {
-                if (emergency[i] < emergency[j]) {
-                    int temp = emergency[i];
-                    emergency[i] = emergency[j];
-                    emergency[j] = temp;
-                }
-            }
-        }
+//        for (int i = 0; i < emergency.length; i++) {
+//            for (int j = i + 1; j < emergency.length; j++) {
+//                if (emergency[i] < emergency[j]) {
+//                    int temp = emergency[i];
+//                    emergency[i] = emergency[j];
+//                    emergency[j] = temp;
+//                }
+//            }
+//        }
+        Integer[] list = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
+        Arrays.sort(list, Collections.reverseOrder());
+        emergency = Arrays.stream(list).mapToInt(Integer::valueOf).toArray();
 
         int v = 1;
         for (int e : emergency) {
