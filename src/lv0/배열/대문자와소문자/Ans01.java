ecce6cabe6fe41b5d6dcf9d175775f310dffec45
KHE <99ant@yonsei.ac.kr>
2025-07-01 15:20:48 +0900

    UpperCase/LowerCase

---
+++ src/lv0/배열/대문자와소문자/Ans01.java
@@ -0,0 +1,23 @@
+package lv0.배열.대문자와소문자;
+
+public class Ans01 {
+    public static void main(String[] args) {
+        String my_string = "cccCCC";
+        System.out.println(solution(my_string));
+    }
+
+    public static String solution(String my_string) {
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (Character.isUpperCase(c)) {
+                answer += Character.toLowerCase(c);
+            } else {
+                answer += Character.toUpperCase(c);
+            }
+        }
+
+        return answer;
+    }
+}
